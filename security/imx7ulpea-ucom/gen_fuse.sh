8042a51aaaed72558129666cfdbfb62ba542f935
Tim Anderson <tim.anderson@foundries.io>
2021-03-11 09:27:18 -0700

    security: imx7ulpea-ucom: Add generation scripts for fuse and close
    Signed-off-by: Tim Anderson <tim.anderson@foundries.io>

---
+++ security/imx7ulpea-ucom/gen_fuse.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# Copyright (C) 2021 Foundries.IO
+#
+# SPDX-License-Identifier: MIT
+#
+
+DIR=$(dirname $0)
+FILE=${FUSE_DEST:-${DIR}/fuse.uuu}
+FUSEBIN=${FUSEBIN:-cst-3.3.1/crts/SRK_1_2_3_4_fuse.bin}
+
+usage() {
+    echo -e "Usage: $0 [-s source_file] [-d destination_file]
+where:
+   source_file defaults to cst-3.3.1/crts/SRK_1_2_3_4_fuse.bin
+   destination file defaults to ${DIR}/fuse.uuu
+" 1>&2
+    exit 1
+}
+
+while getopts ":s:d:" arg; do
+    case "${arg}" in
+        s)
+            FUSEBIN="${OPTARG}"
+            ;;
+        d)
+            FILE="${OPTARG}"
+            ;;
+        s)
+            usage
+            ;;
+    esac
+done
+
+DIR=$(dirname ${FILE})
+shift $((OPTIND-1))
+
+if [ $# -gt 0 ]
+then
+    echo "Too many arguments" 1>&1
+    usage
+fi
+
+if [ ! -f "${FUSEBIN}" ]
+then
+    echo "source file '${FUSEBIN}' not found" 1>&2
+    usage
+fi
+
+mkdir -p "${DIR}"
+
+if [ ! -d "${DIR}" ]
+then
+    echo "destination directory '${DIR}' missing"
+fi
+
+
+(cat << EOF
+uuu_version 1.2.39
+
+SDP: boot -f SPL-mfgtool.signed
+
+SDPU: delay 1000
+SDPV: write -f u-boot-mfgtool.itb
+SDPU: jump
+
+EOF
+HASH=($(hexdump -e '/4 "0x"' -e '/4 "%X""\n"' ${FUSEBIN}))
+for bank in 5 6
+do
+    for idx in $(seq 0 7)
+    do
+        echo "FB: ucmd fuse prog -y ${bank} ${idx} ${HASH[${idx}]}"
+    done
+    echo
+done
+echo "FB: acmd reset"
+echo
+echo "FB: DONE") > ${FILE}
