889eebf8ee46b41008c9863ec956bd316f97db5e
Ricardo Salveti <ricardo@foundries.io>
2022-06-10 11:15:53 -0300

    zynqmp: check_bootbin: fix verify line
    Verify line was incorrect, fix by using the right flag.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- security/zynqmp/check_bootbin.sh
+++ security/zynqmp/check_bootbin.sh
@@ -9,5 +9,5 @@ read -p "Show the bootheader information [enter] " foo
 ./bootgen-spl -arch zynqmp -read boot.bin
 
 read -p "Verify the bootable image [enter] "
-./bootgen-spl -arch zynqmp -image boot.bin -verify
+./bootgen-spl -arch zynqmp -verify boot.bin
 
